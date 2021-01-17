a78413d369d2a84921a773d52f45e7a6e510176a
a <a@b.com>
2021-01-17 12:22:49 +0100

    deleted old versions

--- OOPsilbenSpiel/silbe.py
+++
@@ -1,9 +0,0 @@
-import pygame, sys, time, random
-import tkinter as tk
-from tkinter.constants import *
-from pygame.locals import *
-from OOPsilbenSpiel import game
-
-class Silbe:
-    def __init__(self):
-        pass
