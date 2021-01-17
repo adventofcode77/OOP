6e471dd7fb6e2494062daf40f6593e7ef6802ef4
a <a@b.com>
2021-01-17 12:13:31 +0100

    working syl fall

---
+++ OOPsilbenSpiel7/game.py
@@ -0,0 +1,33 @@
+import pygame as pg
+import sys, time, random
+from pygame.locals import *
+from OOPsilbenSpiel7 import spieler
+from OOPsilbenSpiel7 import setup
+from OOPsilbenSpiel7 import bank
+from OOPsilbenSpiel7 import silbe
+
+class Game:
+
+    def __init__(self):
+        self.user = spieler.Spieler()
+        self.bank = bank.Bank()
+        print("game works")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     pass
+
---
+++ OOPsilbenSpiel7/main.py
@@ -0,0 +1,23 @@
+import pygame as pg
+import sys, time, random
+from pygame.locals import *
+from OOPsilbenSpiel7 import spieler
+from OOPsilbenSpiel7 import setup
+from OOPsilbenSpiel7 import bank
+from OOPsilbenSpiel7 import silbe
+from OOPsilbenSpiel7 import game
+
+
+
+def main():
+    setup.initiate()
+    game1 = game.Game()
+    game1.bank.runterfallen()
+
+
+
+main()
+
+
+# if __name__ == '__main__':
+#     main()
