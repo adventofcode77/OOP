68c378bc0465cb98765d509fd95477a73667a489
a <a@b.com>
2021-01-18 08:06:48 +0100

    setup has update function which saves player object (so it doesn't get updated at more than one location)

--- OOPsilbenSpiel7/game.py
+++ OOPsilbenSpiel7/game.py
@@ -23,14 +23,13 @@ class Game:
         rects = bank.get_rects()
         for i in range(len(bank.flat)):
             setup.things_on_screen = [] #start from empty screen
-            print(setup.things_on_screen)
+
             for j in range(len(rects[:i])):
 
                 for event in pg.event.get():
                     if event.type == KEYDOWN:
                         player.onemove(event)
-                        player.show() #spieler bewegungen zeigen
-                        #self.screen_update()
+                        player.show() #den dinamischen objekt in things_on_screen speichern
 
                 rects[j].show()
                 #self.screen_update() #bisherige silben zeigen
