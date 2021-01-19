97590b9682a833ad505063d92c43bec4d7a9b7bd
lena <ipbkoa20@gmail.com>
2021-01-19 20:59:19 +0100

    works but flickers? syls don't work

--- OOPsilbenSpiel7/game.py
+++ OOPsilbenSpiel7/game.py
@@ -14,34 +14,28 @@ class Game:
         self.bank = bank.Bank()
         self.rects = self.bank.get_rects()
 
-    def start(self):
-        syl_abstand = 0
-        counter = 0
-        while True:
-            counter += 1
-            setup.clock.tick(setup.fps)
-            player = self.player
-            bank = self.bank
+    def move_syls(self,neu,alt):
+        alte_silbe = self.rects[alt]
+        alte_silbe.show()
+        alte_silbe.move()
+        neue_silbe = self.rects[neu]
+        neue_silbe.show()
+        neue_silbe.move()
+        setup.screen_update(self.player)
+        return [neu if neu>alt+1 else neu+1,alt+1]
 
-            for stuff in event.get():
-                if stuff.type == QUIT:
-                    quit()
+    def loop(self,neu,alt,counter):
+        if neu == len(self.rects)-1:
+            exit()
+        print(counter)
+        counter += 1
+        setup.clock.tick(setup.fps)
 
-            keys = key.get_pressed()
-            player.slide(keys)
+        for stuff in event.get():
+            if stuff.type == QUIT:
+                quit()
+        self.player.slide()
+        self.loop(self.move_syls(neu,alt)[0],self.move_syls(neu,alt)[1],counter)
+        #self.loop(neu if neu>alt+1 else neu+1,alt+1,counter)
 
-            if counter %30 == 0:
-
-            for i in range(len(bank.flat)):
-                print("screen ", syl_abstand)
-                syl_abstand += 1
-                silben = self.rects
-                for j in range(len(silben[:i])):
-                    alte_silbe = silben[j]
-                    alte_silbe.show()
-                    alte_silbe.move()
-                neue_silbe = silben[i]
-                neue_silbe.show()
-                neue_silbe.move()
-                setup.screen_update(player)
 
