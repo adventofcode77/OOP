400d23f68735c2daaae26954eb0716ed99ae3ae8
lena <ipbkoa20@gmail.com>
2021-01-23 00:27:35 +0100

    player can collect syls. why doesn't the warning print appear?

--- OOPsilbenSpiel7/silbe.py
+++ OOPsilbenSpiel7/silbe.py
@@ -6,12 +6,10 @@ from OOPsilbenSpiel7 import setup
 class Silbe: #do with sprites
     def __init__(self,silbe,x,y):
         self.inhalt = silbe
-        length = len(silbe)*10
-        if x == 0:
-            x = random.randrange(length,500-length,50)
-        self.rect = pg.Rect(x,y,length,20) # or text.get_rect()?
         font = pg.font.SysFont("Arial",20)
         self.image = font.render(silbe,False,setup.black)
+        self.rect = self.image.get_rect() # or text.get_rect()?
+        self.rect.x = random.randrange(0,500-self.rect.w,50)
         self.speed = 3
         self.on = False
 
