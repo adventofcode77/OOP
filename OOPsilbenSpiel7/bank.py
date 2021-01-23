f973a9a8a7fe188ab0e50a2d18df47aacf5bb18a
a <a@b.com>
2021-01-23 22:48:08 +0100

    spaghetti dict with name as key and meaning and syls as value

--- OOPsilbenSpiel7/bank.py
+++ OOPsilbenSpiel7/bank.py
@@ -9,17 +9,21 @@ import numpy
 class Bank:
     def __init__(self):
         self.rect = pg.Rect(0,0,20,20)
-        self.array = setup.get_bank()
-        self.flat = list(numpy.concatenate(self.array).flat)
+        self.dictwithkeyname = setup.get_bank()
+        self.listofsyls = []
+        self.flat = []
         self.font = pg.font.SysFont("Arial",30)
 
 
     def get_rects(self):
         rects = []
-        for syl in self.flat:
-            x = random.randrange(50,450,50)
-            syl = silbe.Silbe(syl)
-            rects.append(syl)
+        for entry in self.dictwithkeyname:
+            sylslist = self.dictwithkeyname[entry][1]
+            for silbe2 in sylslist:
+                x = random.randrange(50,450,50)
+                syl = silbe.Silbe(silbe2)
+                rects.append(syl)
+        print("get rects ",[a.inhalt for a in rects])
         return rects
 
     def compose(self):
