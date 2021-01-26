dcfd5fd907f65563af91072fb74ca1ba57c6678e
a <a@b.com>
2021-01-27 00:31:12 +0100

    cleanup

--- OOPsilbenSpiel7/bank.py
+++ OOPsilbenSpiel7/bank.py
@@ -2,7 +2,7 @@ import pygame as pg
 from pygame import *
 import sys, time, random
 from pygame.locals import *
-from OOPsilbenSpiel7 import silbe
+from OOPsilbenSpiel7 import silbe, parsewikt
 from OOPsilbenSpiel7 import setup
 from OOPsilbenSpiel7 import word
 from OOPsilbenSpiel7 import spieler
@@ -11,24 +11,32 @@ class Bank:
     def __init__(self):
         self.spieler = spieler.Spieler()
         self.rect = pg.Rect(0,0,20,20)
-        self.dictwithkeyname = self.spieler.base.get_bank()
+        self.dictwithkeyname = self.get_bank2()
         self.txtsyls = self.randomize_syls()
         self.flat = []
         self.font = pg.font.SysFont("Arial",30)
-        self.words = self.get_words()
-        self.silben = self.get_silben()
+        print("works before init words")
         self.test = 3
+        self.words = self.get_words()
+        print("just in case")
+        self.silben = self.get_silben()
+        print("init bank")
 
 
     def get_words(self):
         words = []
+        c = 0
         for entry in self.dictwithkeyname:
+            self.test += 1
             name = entry
             meaning = self.dictwithkeyname[entry][0]
             syls = self.dictwithkeyname[entry][1]
             aword = word.Word(name,meaning,syls)
+            print("inside get words before append")
             words.append(aword)
+            print(self.test, name)
         #print("get  ",[a.name for a in words])
+        print("inside get words return")
         return words
 
     def get_silben(self):
@@ -52,3 +60,9 @@ class Bank:
         #return all_syls
 
 
+    def get_bank2(self):
+        file_path = '/Users/ellie/Downloads/dewiktionary-20210101-pages-articles-multistream-2.xml'
+        parser = parsewikt.Parse(file_path)
+        bank2 = parser.parsed
+        print(bank2, "\n\nnext\n\n")
+        return bank2
--- OOPsilbenSpiel7/setup.py
+++ OOPsilbenSpiel7/setup.py
@@ -1,11 +1,4 @@
 import pygame as pg
-import sys, time, random
-from pygame.locals import *
-import linecache
-from wiktionary_de_parser import Parser
-import parsewikt3
-import parsewikt
-import numpy
 
 class Settings:
     def __init__(self):
@@ -13,33 +6,8 @@ class Settings:
         self.white = (255,255,255)
         self.zuff = (200,200,200)
         self.lila = (125,33,200)
-        self.clock = pg.time.Clock()
         self.fps = 60
-        self.things_on_screen = []
-        self.pause = False
         self.screenw, self.screenh = 500,500
         self.right = self.screenw//6
         self.down = self.screenh//12
-        self.screen = pg.display.set_mode((self.screenh,self.screenw))
         self.font = pg.font.SysFont("Arial",30)
-        self.file_path = '/Users/ellie/Downloads/dewiktionary-20210101-pages-articles-multistream-2.xml'
-        self.parser = parsewikt.Parse()
-        self.parsed2 = self.parser.parsed
-        print(self.parsed2, "\n\nnext\n\n")
-
-    def screen_update_and_move(self,allsyls,current_syl,player): # after every changed object
-        self.screen.fill(self.zuff)
-        for i in range(current_syl):
-            syllable = allsyls[i]
-            if syllable.visible == True:
-                self.screen.blit(syllable.image,syllable.rect) #draw function?
-            syllable.rect.y += syllable.speed
-        self.screen.blit(player.image,player.rect)
-        pg.display.flip()
-
-    def get_bank(self):
-        bank = self.parsed2
-        return bank
-
-
-
