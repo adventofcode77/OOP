271a8020fe273f95801f60102694367627dc7883
a <a@b.com>
2021-01-27 11:26:34 +0100

    classes inherit the variables in settings

--- OOPsilbenSpiel7/main.py
+++ OOPsilbenSpiel7/main.py
@@ -1,23 +1,23 @@
 import pygame as pg
 import sys, time, random
-from pygame.locals import *
-import spieler
-import setup
-import bank
-import silbe
+from pygame import *
 import game
 
+class Main:
+    def __init__(self):
+        self.newgame(0)
 
+    def newgame(self,score):
+        pg.init()
+        game1 = game.Game()
+        loop = game1.gameloop()
+        score += loop
+        print("score:",score)
+        newgame = input("Game over. New game?")
+        if newgame == "y":
+            self.newgame(score)
+        else:
+            exit()
 
-def main():
-    pg.init()
-    game1 = game.Game()
-    game1.gameloop()
-
-
-
-main()
-
-
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+     Main()
--- OOPsilbenSpiel7/silbe.py
+++ OOPsilbenSpiel7/silbe.py
@@ -1,17 +1,16 @@
 import pygame as pg
-import sys, time, random
-from pygame.locals import *
-from OOPsilbenSpiel7 import setup
+import random
 from OOPsilbenSpiel7 import spieler
+import globale_variablen
 
-class Silbe: #do with sprites
-    def __init__(self,it,word,bit):
+class Silbe(globale_variablen.Settings): #do with sprites
+    def __init__(self,it,word,bit): #or make it inherit from word
         #self.spieler.base = setup.Settings()
         self.spieler = spieler.Spieler()
         self.inhalt = it
         self.word = word
         font = pg.font.SysFont("Arial",20)
-        self.image = font.render(it,False,self.spieler.base.black)
+        self.image = font.render(it, False, self.spieler.black)
         self.rect = self.image.get_rect() # or text.get_rect()?
         self.rect.x = random.randrange(0,500-self.rect.w,50)
         self.speed = 3
