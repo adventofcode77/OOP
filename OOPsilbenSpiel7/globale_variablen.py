f69cc66d3e760483bf0d902d7ea06eaa68b338a8
a <a@b.com>
2021-02-06 00:06:35 +0100

    loop breaks when there are too few syls

--- OOPsilbenSpiel7/globale_variablen.py
+++ OOPsilbenSpiel7/globale_variablen.py
@@ -20,7 +20,7 @@ class Settings:
         self.font = font.SysFont("Arial",20)
         self.bigfont = font.SysFont("Arial", 30)
         self.deffont = font.SysFont("Arial",15)
-        self.invisible = self.font.render("shh", False, self.white)
+        self.invisible = self.font.render("o", False, self.gold)
 
     def get_bits(self,string,length):
         definition = string
