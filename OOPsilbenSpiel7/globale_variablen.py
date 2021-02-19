4c12d3e406c012e0cf03a4fd5cc7f59345eb7afd
a <a@b.com>
2021-02-19 21:09:05 +0100

    many little commented changes

--- OOPsilbenSpiel7/globale_variablen.py
+++ OOPsilbenSpiel7/globale_variablen.py
@@ -16,6 +16,7 @@ class Settings:
         self.yellow = (255,255,0)
         self.fps = 30
         self.screenw, self.screenh = 500,500
+        # why does increasing the screen size from 500,500 lead to blank screen and player escaping it?
         self.right = self.screenw//6
         self.down = self.screenh//12
         self.font = font.SysFont("Arial",20)
