4a383823d6ca4447bdccdef761b641f0937d3012
lena <ipbkoa20@gmail.com>
2021-02-26 10:51:22 +0100

    quick save current state

--- OOPsilbenSpiel7/word.py
+++ OOPsilbenSpiel7/word.py
@@ -5,14 +5,17 @@ import globale_variablen
 import math
 
 class Word():
+    all_syls = []
     def __init__(self, key, meaning,txtsilben,worder,totalsyls,info):
         super().__init__()
         self.info = info
         self.totalsyls = totalsyls
         self.worder = worder
         self.name = key
+        self.meaning = [a for a in meaning.split() if a is not None] # remove the empty strings
         self.txtsilben = txtsilben
-        self.bits = self.info.get_bits(meaning, len(self.txtsilben))
+        Word.all_syls.append(txtsilben)
+        self.bits = self.info.get_bits(self.meaning, len(self.txtsilben))
         self.syls = self.make_silben()
         self.image = self.info.font.render(self.name, False, self.info.black)
         self.rect = self.image.get_rect() # draw_rect()?
