845cec614948b52de51b36d1c7f040e0227e729b
lena <ipbkoa20@gmail.com>
2021-03-23 14:31:01 +0100

    removed english wiktionary dump

--- Worterror/main.py
+++ Worterror/main.py
@@ -5,7 +5,7 @@ import game
 
 class Main:
     #TODO make a script that downloads the wiktionary dump/imports the parser and configures their paths
-    file_paths = ['/Users/ellie/Downloads/dewiktionary-20210101-pages-articles-multistream-2.xml',"/Users/ellie/Downloads/enwiktionary-20210220-pages-articles-multistream.xml"] # backup
+    file_paths = ['/Users/ellie/Downloads/dewiktionary-20210101-pages-articles-multistream-2.xml'] # ,"/Users/ellie/Downloads/enwiktionary-20210220-pages-articles-multistream.xml"] # backup
     code_de = ["Verliebte Dame, deren Herz schlägt für den Bauern"] #random.choice([f"Der Turm hat vier Ebenen","Gehe zum Feld 6 bei 6 auf dem Schachbrett","Achte auf den Zeichen an der Decke","Der Schlüssel steht unter Angriff","Eine figur wird mit dem Schlüssel rausfliegen","Du brauchst mehr als sechs Schritte","5 links, 3 gerade aus, 2 rechts"])
     binary_code = "01001001"
     def __init__(self):
