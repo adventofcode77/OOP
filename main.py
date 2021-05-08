b4441afeb60a6a659e98ca565b0ecda12e2eab49
e <ipbkoa20@gmail.com>
2021-05-08 21:11:37 +0200

    Changed the secret codes.

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import woerterbuch
 class Main:
     #TODO make a script that downloads the wiktionary dump/imports the parser and configures their paths
     file_paths = ['/Users/ellie/Downloads/dewiktionary-20210101-pages-articles-multistream-2.xml'] # ,"/Users/ellie/Downloads/enwiktionary-20210220-pages-articles-multistream.xml"] # backup
-    code_de = ["Verliebte"] # Dame, deren Herz schlägt für den Bauern"] #random.choice([f"Der Turm hat vier Ebenen","Gehe zum Feld 6 bei 6 auf dem Schachbrett","Achte auf den Zeichen an der Decke","Der Schlüssel steht unter Angriff","Eine figur wird mit dem Schlüssel rausfliegen","Du brauchst mehr als sechs Schritte","5 links, 3 gerade aus, 2 rechts"])
+    code_de = ["Das Herz der Verliebten Dame schlägt für den Bauern"] #random.choice([f"Der Turm hat vier Ebenen","Gehe zum Feld 6 bei 6 auf dem Schachbrett","Achte auf den Zeichen an der Decke","Der Schlüssel steht unter Angriff","Eine figur wird mit dem Schlüssel rausfliegen","Du brauchst mehr als sechs Schritte","5 links, 3 gerade aus, 2 rechts"])
     dict = woerterbuch.Woerterbuch(file_paths[0]).quick_get(50)
-    binary_code = "01001001"
+    binary_code = "000011001"
 
     def __init__(self):
         self.newgame(Main.code_de, Main.file_paths, Main.binary_code)
