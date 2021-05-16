c614c9fd129f938405619bf2e822e51b16c9ab27
e <ipbkoa20@gmail.com>
2021-05-16 22:43:37 +0200

    created more unit tests

--- Woerterbuchspiel/test_game.py
+++ Woerterbuchspiel/test_game.py
@@ -6,3 +6,13 @@ class TestGame(TestCase):
     def test_get_syl_returns_None(self):
         syl = game.Game.get_syl(0, 0, [], 10)
         assert syl is None
+
+    def test_get_syl_returns_element_0(self):
+        syl = game.Game.get_syl(0, 0, [2,3], 1)
+        assert syl == 2
+
+    def test_get_syl_returns_the_first_element_in_second_column(self):
+        assert game.Game.get_syl(1, 0, [2, 3, 4, 5, 6], 3) == 5
+        assert game.Game.get_syl(1, 1, [2, 3, 4, 5, 6], 3) == 6
+        assert game.Game.get_syl(1, 2, [2, 3, 4, 5, 6], 3) is None
+
